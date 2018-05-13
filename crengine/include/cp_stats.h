6b6d30cd502790a37736531c232f5b47cc9128aa
Aleksey Chernov <valexlin@gmail.com>
2018-05-13 22:56:01 +0400

    Pretect file cp_stats.h from multiple 'include'.

--- crengine/include/cp_stats.h
+++ crengine/include/cp_stats.h
@@ -1,3 +1,6 @@
+#ifndef __CP_STATS_H_INCLUDED__
+#define __CP_STATS_H_INCLUDED__
+
 typedef struct {
    unsigned char ch1;
    unsigned char ch2;
@@ -9,3 +12,5 @@ typedef struct {
    unsigned char ch2;
    int      count;
 } dbl_char_stat_long_t;
+
+#endif	// __CP_STATS_H_INCLUDED__
